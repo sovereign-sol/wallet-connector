f4106895a348ef41daff47c83bb00e49825b614f
sovereign-sol <146667357+sovereign-sol@users.noreply.github.com>
2024-09-08 12:20:46 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Wallet } from './components/Wallets'
+
+function App() {
+
+  return (
+    <div className="w-full h-[100vh] flex items-center justify-center">
+    <Wallet />
+    </div>
+  )
+}
+
+export default App
